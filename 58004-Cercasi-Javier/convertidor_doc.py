00aeac48c38a64c173cc0b9a6b4a0bbde1f021c2
javicercasi <jacercasii@gmail.com>
2021-04-06 18:06:11 -0300

    Proyecto

--- 58004-Cercasi-Javier/convertidor_doc.py
+++ 58004-Cercasi-Javier/convertidor_doc.py
@@ -2,12 +2,12 @@ import os
 import subprocess
 from pdf2docx import Converter
 
-def pdf_to_word(file=0):
+def pdf_to_word(file=0, ):
     file = "enunciado.pdf"
-    path_input = '/home/javi/Final_Computacion/58004-Cercasi-Javier/pdfs/'
-    path_output = '/home/javi/Final_Computacion/58004-Cercasi-Javier/pdfs/'
-    cv = Converter(path_input+file)
-    cv.convert(path_output+file.split(".")[0]+'.docx', start=0, end=None)
+    #path_input = '/home/javi/Final_Computacion/58004-Cercasi-Javier/pdfs/'
+    #path_output = '/home/javi/Final_Computacion/58004-Cercasi-Javier/pdfs/'
+    cv = Converter(os.getcwd()+file)
+    cv.convert(os.getcwd()+file.split(".")[0]+'.docx', start=0, end=None)
     cv.close()
 
 pdf_to_word()
