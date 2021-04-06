4c891dc96049ad4c47b99c14075b4f287505d8f6
javicercasi <jacercasii@gmail.com>
2021-04-06 18:25:56 -0300

    Proyecto

--- 58004-Cercasi-Javier/convertidor_doc.py
+++ 58004-Cercasi-Javier/convertidor_doc.py
@@ -6,11 +6,9 @@ def pdf_to_word(file=0, ):
     file = "enunciado.pdf"
     #path_input = '/home/javi/Final_Computacion/58004-Cercasi-Javier/pdfs/'
     #path_output = '/home/javi/Final_Computacion/58004-Cercasi-Javier/pdfs/'
-    cv = Converter(os.getcwd()+file)
-    cv.convert(os.getcwd()+file.split(".")[0]+'.docx', start=0, end=None)
+    cv = Converter(os.getcwd()+"/"+file)
+    cv.convert(os.getcwd()+"/"+file.split(".")[0]+'.docx', start=0, end=None)
     cv.close()
-
-pdf_to_word()
 
 def word_to_pdf():
     output = subprocess.check_output(['unoconv', '-f', 'pdf' ,'/home/javi/Final_Computacion/58004-Cercasi-Javier/pdfs/enunciado.pdf.docx'])
